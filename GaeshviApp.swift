2245e85d216e669cd627906f07d4938fa01b9d47
Levan Loladze <levan.loladzew@gmail.com>
2025-06-07 14:38:29 +0400

    Initial commit

---
+++ GaeshviApp.swift
@@ -0,0 +1,17 @@
+//
+//  GaeshviApp.swift
+//  Gaeshvi
+//
+//  Created by Levan Loladze on 07.06.25.
+//
+
+import SwiftUI
+
+@main
+struct GaeshviApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
